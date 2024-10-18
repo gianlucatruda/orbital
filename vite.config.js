69071178d4b664e8629a4dc06201846794b8a020
Gianluca Truda <dev@gianluca.ai>
2024-10-18 18:56:16 +0100

    Project restructure, updated build scripts and configs.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 export default {
-  root: "src/",
+  root: "UI/",
   publicDir: "../static/",
   base: "./",
+  server: {
+    fs: {
+      allow: ['..'],
+    },
+  },
+  build: {
+    target: 'esnext',
+    outDir: "../dist",
+  },
+  optimizeDeps: {
+    exclude: ['engine'],
+  },
 };
